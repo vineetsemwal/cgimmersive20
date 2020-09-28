377c0182287d9a134c859a32174702be5846c816
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-28 14:23:09 +0530

    few improvements

--- randomexperiments/src/Student.java
+++ randomexperiments/src/Student.java
@@ -18,11 +18,18 @@ public class Student {
 	int id;
 	String name;
 	
+	void setId(int id) {
+		this.id=id;
+	}
+	
 	int getId() {
 		return id;
 	}
 	
-		
+	void setName(String name) {
+		this.name=name;
+	}
+	
 	String getName() {
 		return name;
 	}
--- randomexperiments/src/StudentMain.java
+++ randomexperiments/src/StudentMain.java
@@ -3,13 +3,18 @@ public class StudentMain {
 	
 	public static void main(String args[] ) {	
 		Student student1=new Student(1,"shivam");	
-		student1.name="shivay";
-		Student student2=new Student();
-		student2.id=2;
-		student2.name="abhishek";
-		String name2=student2.getName();
-		System.out.println("student 1 detail is "+student1.id+ " "+student1.name);
-		System.out.println("student 2 detail is "+student2.id+ " "+student2.name);
+		Student student2=new Student(2,"abhishek");
+		student2.setName("aniket");
+	    Student students[]=new Student[2];
+	    students[0]=student1;
+	    students[1]=student2;
+		for(int i=0;i <students.length;i++)
+	    {
+			Student iterated=students[i];
+			int id=iterated.getId();
+			String name=iterated.getName();
+			System.out.println("student detail is "+id+ " "+name);
+	    }
 		
 	}
 
