55d78472fb4e232d2786a14dd47087d9f5b4bb94
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-28 14:41:00 +0530

    more improvements

--- randomexperiments/src/StudentMain.java
+++ randomexperiments/src/StudentMain.java
@@ -1,31 +1,30 @@
 
 public class StudentMain {
-	
+    Student students[]=new Student[2];//field of StudentMain object
+
 	public static void main(String args[] ) {	
 		StudentMain mainObj=new StudentMain();
 		mainObj.execute();
 	}
 	
+	
 	public void execute() {
 		Student student1=new Student(1,"shivam");	
 		Student student2=new Student(2,"abhishek");
 		student2.setName("aniket");
-	    Student students[]=new Student[2];
 	    students[0]=student1;
-	    students[1]=student2;
-	    for(int i=0;i<students.length;i++) {
-	    	Student iterated=students[i];
-	    	int id=iterated.getId();
-	    	String name=iterated.getName();
-			System.out.println("student detail is "+id+ " "+name);
-	    }
+	    students[1]=student2;	  
+	    displayStudents();
+	   
+	}
+	
+	public void displayStudents() {
 		for(Student iterated:students)
 	    {
 			int id=iterated.getId();
 			String name=iterated.getName();
 			System.out.println("student detail is "+id+ " "+name);
 	    }
-		
 	}
 
 }
