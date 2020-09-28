a82984354f0212a689334909febca8b4df54b8e9
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-28 14:31:49 +0530

    code moved out of main

--- randomexperiments/src/StudentMain.java
+++ randomexperiments/src/StudentMain.java
@@ -2,6 +2,11 @@
 public class StudentMain {
 	
 	public static void main(String args[] ) {	
+		StudentMain mainObj=new StudentMain();
+		mainObj.execute();
+	}
+	
+	public void execute() {
 		Student student1=new Student(1,"shivam");	
 		Student student2=new Student(2,"abhishek");
 		student2.setName("aniket");
@@ -10,6 +15,7 @@ public class StudentMain {
 	    students[1]=student2;
 	    for(int i=0;i<students.length;i++) {
 	    	Student iterated=students[i];
+	    	int id=iterated.getId();
 	    	String name=iterated.getName();
 			System.out.println("student detail is "+id+ " "+name);
 	    }
