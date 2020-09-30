a633612ead89e890ea1164e3ec687d352f63c13c
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-30 15:46:02 +0530

    another custom exception example added

---
+++ randomexperiments/src/randomexperiments/exceptionhandling/NoVowelFoundException.java
@@ -0,0 +1,8 @@
+package randomexperiments.exceptionhandling;
+
+public class NoVowelFoundException extends Exception{
+	
+ public NoVowelFoundException(String msg) {
+	 super(msg);
+ }
+}
---
+++ randomexperiments/src/randomexperiments/exceptionhandling/VowelsExample.java
@@ -0,0 +1,40 @@
+package randomexperiments.exceptionhandling;
+
+public class VowelsExample {
+	
+	public static void main(String args[]) {
+		try {
+			VowelsExample example=new VowelsExample();
+			String input="zzzz";
+			String vowels=example.findVowels(input);
+			System.out.println("vowels="+vowels);
+			char firstVowel=vowels.charAt(0);
+			System.out.println("first vowel found is "+firstVowel);
+		}
+		catch(InvalidArgumentException e) {
+		  System.out.println("argument is null or empty so we cant find vowels");	
+		}
+		catch(NoVowelFoundException e) {
+			System.out.println("no vowel found in input so i cant tell you about first vowel");
+		}
+	}
+	
+	String findVowels(String input) throws InvalidArgumentException,NoVowelFoundException {
+		if(input==null || input.isEmpty()) {
+			throw new InvalidArgumentException("input is null or empty");
+		}
+		String vowels="";
+		for(int index=0;index<input.length();index++) {
+			char ch=input.charAt(index);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u') {
+				vowels=vowels+ch;
+			}
+		}
+		if(vowels.isEmpty()) {
+			throw new NoVowelFoundException("no vowel found in "+input);
+		}
+		
+		return vowels;
+	}
+
+}
