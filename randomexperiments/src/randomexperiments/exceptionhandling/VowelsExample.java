4afcd8c9e90e9c4ee5ca0e387d93bbd97cf7cc11
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-30 16:43:43 +0530

    finally example added

--- randomexperiments/src/randomexperiments/exceptionhandling/VowelsExample.java
+++ randomexperiments/src/randomexperiments/exceptionhandling/VowelsExample.java
@@ -1,44 +1,47 @@
 package randomexperiments.exceptionhandling;
 
 public class VowelsExample {
-	
+
 	public static void main(String args[]) {
 		try {
-			VowelsExample example=new VowelsExample();
-			String input="zzzz";
-			String vowels=example.findVowels(input);
-			System.out.println("vowels="+vowels);
-			char firstVowel=vowels.charAt(0);
-			System.out.println("first vowel found is "+firstVowel);
+			VowelsExample example = new VowelsExample();
+			String input = "zzzz";
+			String vowels = example.findVowels(input);
+			System.out.println("vowels=" + vowels);
+			char firstVowel = vowels.charAt(0);
+			System.out.println("first vowel found is " + firstVowel);
 		}
-		
-		catch(InvalidArgumentException e) {
-		  System.out.println("argument is null or empty so we cant find vowels");	
-		}
-		catch(NoVowelFoundException e) {
+
+		catch (InvalidArgumentException e) {
+			System.out.println("argument is null or empty so we cant find vowels");
+		} catch (NoVowelFoundException e) {
 			System.out.println("no vowel found in input so i cant tell you about first vowel");
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("something went wrong");
+
 		}
 		
+		finally {
+			System.out.println("bye bye");
+		}
+
 	}
-	
-	String findVowels(String input)  {
-		if(input==null || input.isEmpty()) {
+
+	String findVowels(String input) {
+		if (input == null || input.isEmpty()) {
 			throw new InvalidArgumentException("input is null or empty");
 		}
-		String vowels="";
-		for(int index=0;index<input.length();index++) {
-			char ch=input.charAt(index);
-			if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u') {
-				vowels=vowels+ch;
+		String vowels = "";
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vowels = vowels + ch;
 			}
 		}
-		if(vowels.isEmpty()) {
-			throw new NoVowelFoundException("no vowel found in "+input);
+		if (vowels.isEmpty()) {
+			throw new NoVowelFoundException("no vowel found in " + input);
 		}
-		
+
 		return vowels;
 	}
 
