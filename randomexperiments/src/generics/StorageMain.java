83028b0857c86c083a0fede106de76f948bd920c
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-01 12:50:43 +0530

    formatting

--- randomexperiments/src/generics/StorageMain.java
+++ randomexperiments/src/generics/StorageMain.java
@@ -3,11 +3,11 @@ package generics;
 public class StorageMain {
 
 	public static void main(String args[]) {
-		Integer value=20;
-		Storage<Integer> storage=new Storage<Integer>();
+		Integer value = 20;
+		Storage<Integer> storage = new Storage<Integer>();
 		storage.setValue(value);
-	    System.out.println("added integer value to storage");
-		Integer fetched=storage.getValue();
-	
+		System.out.println("added integer value to storage");
+		Integer fetched = storage.getValue();
+
 	}
 }
