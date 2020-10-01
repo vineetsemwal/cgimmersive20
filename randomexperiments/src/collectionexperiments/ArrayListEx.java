fb903561afa8476800aff1688b320afbcada8656
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-01 14:46:06 +0530

    formatting

--- randomexperiments/src/collectionexperiments/ArrayListEx.java
+++ randomexperiments/src/collectionexperiments/ArrayListEx.java
@@ -6,25 +6,25 @@ public class ArrayListEx {
 
 	public static void main(String args[]) {
 		List<String> list = new ArrayList<>();
-		int count=list.size();
-        System.out.println("intial elements count ="+count);
+		int count = list.size();
+		System.out.println("intial elements count =" + count);
 		list.add("shivam");
 		list.add("hashil");
 		list.add("swati");// element added at index 2
 		list.add("vidya");// element add at index 3
-        count=list.size();
-        System.out.println("4 elements added, count ="+count);
-		
+		count = list.size();
+		System.out.println("4 elements added, count =" + count);
+
 		String elementAtIndex2 = list.get(2);
 
 		list.remove(elementAtIndex2);
 		list.remove(1);
-		 System.out.println("removed 2 elements, count ="+count);
-			
-		for(String element:list) {
-		    System.out.println("element="+element);
+		System.out.println("removed 2 elements, count =" + count);
+
+		for (String element : list) {
+			System.out.println("element=" + element);
 		}
-		 
+
 	}
 
 }
