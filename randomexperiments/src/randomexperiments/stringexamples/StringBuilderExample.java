f520a917eb2ea82fe7a72a93693d09f8fcd26274
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-01 11:57:22 +0530

    stringbuilder example added

---
+++ randomexperiments/src/randomexperiments/stringexamples/StringBuilderExample.java
@@ -0,0 +1,11 @@
+package randomexperiments.stringexamples;
+
+public class StringBuilderExample {
+
+	 public static void main(String args[]) {
+		 StringBuilder builder1=new StringBuilder("hydera");
+		 builder1.append("-").append("telangana").append("-india");
+		 String str=builder1.toString();
+		 System.out.println("str="+str);
+	 }
+}
