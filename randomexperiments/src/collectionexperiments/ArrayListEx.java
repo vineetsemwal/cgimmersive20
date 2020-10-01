46dd0ae0b495396dfe8f3b2d635e76697ce99c27
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-01 14:44:51 +0530

    arraylist example added

---
+++ randomexperiments/src/collectionexperiments/ArrayListEx.java
@@ -0,0 +1,30 @@
+package collectionexperiments;
+
+import java.util.*;
+
+public class ArrayListEx {
+
+	public static void main(String args[]) {
+		List<String> list = new ArrayList<>();
+		int count=list.size();
+        System.out.println("intial elements count ="+count);
+		list.add("shivam");
+		list.add("hashil");
+		list.add("swati");// element added at index 2
+		list.add("vidya");// element add at index 3
+        count=list.size();
+        System.out.println("4 elements added, count ="+count);
+		
+		String elementAtIndex2 = list.get(2);
+
+		list.remove(elementAtIndex2);
+		list.remove(1);
+		 System.out.println("removed 2 elements, count ="+count);
+			
+		for(String element:list) {
+		    System.out.println("element="+element);
+		}
+		 
+	}
+
+}
