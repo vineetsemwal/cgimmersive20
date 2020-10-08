6d8ff92bf838a4514efed3fd671a3bdca3cb13df
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-08 12:35:22 +0530

    scheduled thread pool demo added'

--- randomexperiments/src/mt/executors/ExecutorExample.java
+++ randomexperiments/src/mt/executors/ExecutorExample.java
@@ -4,23 +4,26 @@ import mt.ImplementRunnableChild;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorExample {
 
     public static void main(String args[]){
        //ExecutorService executorService= Executors.newFixedThreadPool(1);
        // ExecutorService executorService= Executors.newCachedThreadPool();
-        ExecutorService executorService=Executors.newSingleThreadExecutor();
+       // ExecutorService executorService=Executors.newSingleThreadExecutor();
+        ScheduledExecutorService executorService= Executors.newScheduledThreadPool(2);
         Runnable task1=new ImplementRunnableChild("task1");
         Runnable task2=new ImplementRunnableChild("task2");
         Runnable task3=new ImplementRunnableChild("task3");
         Runnable task4=new ImplementRunnableChild("task4");
-        executorService.submit(task1);
         executorService.submit(task2);
         executorService.submit(task3);
         executorService.submit(task4);
+        executorService.schedule(task1,20, TimeUnit.SECONDS);//scheduled task to execute after 20 seconds
         System.out.println("*******shutting down called");
-       // executorService.shutdown();
+        executorService.shutdown();
 
     }
 }
