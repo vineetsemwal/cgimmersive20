934a2e54c4d99a9e2ccd18fd97348a90d68a63c9
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-08 16:40:35 +0530

    consumer example using lambda

--- randomexperiments/src/lambda/ConsumerExample.java
+++ randomexperiments/src/lambda/ConsumerExample.java
@@ -3,12 +3,22 @@ package lambda;
 public class ConsumerExample {
 
     public static void main(String args[]){
-        IGreet consumer=new IGreet() {
+        IGreet consumer1=new IGreet() {
             @Override
             public void greet(String msg) {
              System.out.println(msg);
             }
         };
-        consumer.greet("welcome to cg training");
+        consumer1.greet("welcome to cg training, using anonymous");
+
+        IGreet consumer2=(msg)->System.out.println(msg);
+        consumer2.greet("welcome to cg training,using expression");
+
+        IGreet consumer3=(msg)->{
+          System.out.println(msg);
+        };
+        consumer3.greet("welcome to cg training,using block");
+
+
     }
 }
