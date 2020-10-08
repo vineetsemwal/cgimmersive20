9233bd493eb6d9aa1f15690addf8c665b9b45906
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-08 12:26:27 +0530

    different thread pools commented in same code

--- randomexperiments/src/mt/executors/ExecutorExample.java
+++ randomexperiments/src/mt/executors/ExecutorExample.java
@@ -8,8 +8,9 @@ import java.util.concurrent.Executors;
 public class ExecutorExample {
 
     public static void main(String args[]){
-      //  ExecutorService executorService= Executors.newFixedThreadPool(2);
-        ExecutorService executorService= Executors.newCachedThreadPool();
+       //ExecutorService executorService= Executors.newFixedThreadPool(1);
+       // ExecutorService executorService= Executors.newCachedThreadPool();
+        ExecutorService executorService=Executors.newSingleThreadExecutor();
         Runnable task1=new ImplementRunnableChild("task1");
         Runnable task2=new ImplementRunnableChild("task2");
         Runnable task3=new ImplementRunnableChild("task3");
@@ -19,7 +20,7 @@ public class ExecutorExample {
         executorService.submit(task3);
         executorService.submit(task4);
         System.out.println("*******shutting down called");
-        executorService.shutdown();
+       // executorService.shutdown();
 
     }
 }
