aaefb9913c318c7896415a5fc8ef2372378368e2
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-08 16:50:47 +0530

    biconsumer example added

--- randomexperiments/src/lambda/BiConsumerExample.java
+++ randomexperiments/src/lambda/BiConsumerExample.java
@@ -3,14 +3,23 @@ package lambda;
 public class BiConsumerExample {
 
     public static void main(String args[]){
-        IConcat concat=new IConcat() {
+        IConcat concat1=new IConcat() {
             @Override
             public void concat(String arg1, String arg2) {
                 String msg=arg1 + arg2;
                 System.out.println(msg);
             }
         };
-        concat.concat("hello", "world");
+        concat1.concat("hello", "world");
+
+        IConcat concat2=(msg1,msg2)->System.out.println(msg1+msg2);
+        concat2.concat("good", "evening");
+
+        IConcat concat3=(msg1,msg2)->{
+          String result=msg1+msg2;
+          System.out.println(result);
+        };
+        concat3.concat("corona","virus");
 
     }
 }
