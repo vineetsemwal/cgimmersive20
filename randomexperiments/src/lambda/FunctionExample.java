8f6add6a88fa1ca88b39f258e676bb144fc892a0
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-08 17:10:16 +0530

    function example added

---
+++ randomexperiments/src/lambda/FunctionExample.java
@@ -0,0 +1,16 @@
+package lambda;
+
+public class FunctionExample {
+    public static void main(String args[]){
+        ILength expression=str->str.length();//lambda expression
+        ILength block=str->{
+            int len=str.length();
+            return len;
+        };
+
+        int len1=expression.length("hello");
+        int len2=block.length("capgemini");
+        System.out.println("expression calcualted length="+len1);
+        System.out.println("block calcualted length="+len2);
+    }
+}
---
+++ randomexperiments/src/lambda/ILength.java
@@ -0,0 +1,6 @@
+package lambda;
+
+@FunctionalInterface
+public interface ILength {
+    int length(String input);
+}
