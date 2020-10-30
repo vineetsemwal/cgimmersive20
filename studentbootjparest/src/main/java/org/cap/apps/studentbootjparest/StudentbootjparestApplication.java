98c276866571b9940d2f8d10befe534be4973364
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-30 16:21:57 +0530

    spring boot rest demo added

---
+++ studentbootjparest/src/main/java/org/cap/apps/studentbootjparest/StudentbootjparestApplication.java
@@ -0,0 +1,13 @@
+package org.cap.apps.studentbootjparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentbootjparestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentbootjparestApplication.class, args);
+	}
+
+}
---
+++ studentbootjparest/src/test/java/org/cap/apps/studentbootjparest/StudentbootjparestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cap.apps.studentbootjparest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentbootjparestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
