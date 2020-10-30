85ca80ab30c0f3bb8ed10da0719755bb5aee75dc
vineetsemwal <vineetsemwal82@gmail.com>
2020-10-30 12:29:16 +0530

    enabletransactionmanagement annotation added

--- studentbootjpa/src/main/java/org/cap/apps/studentbootjpa/ProjectMain.java
+++ studentbootjpa/src/main/java/org/cap/apps/studentbootjpa/ProjectMain.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class ProjectMain {
 
