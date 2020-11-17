d0164c0343d02efa24aec9f86a72a2493d0c2c23
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-17 17:20:55 +0530

    function sued in object

--- clientsidetech/javascript-examples/js/objects.js
+++ clientsidetech/javascript-examples/js/objects.js
@@ -1,11 +1,27 @@
 var user1={
 "id":1,
  "name" : "harshil",
-
  
+ toString:function(){
+ var text= "id is "+this.id + "name is "+this.name;
+  return text;
+ }
 
 };
-var user2={"id":2,"name":"jay"};
+var user2={"id":2,
+"name":"jay",
 
-console.log("object1 ="+user1.id+" "+user1.name);
-console.log("object2="+user2.id+" "+user2.name);
+toString:function(){
+    var text= "id is "+this.id + "name is "+this.name;
+     return text;
+    }
+   
+};
+
+console.log("user1 ="+user1.id+" "+user1.name);
+var toString1=user1.toString();
+console.log("tostring representation="+toString1);
+
+console.log("user2="+user2.id+" "+user2.name);
+var toString2=user2.toString();
+console.log("toString representation="+toString2);
