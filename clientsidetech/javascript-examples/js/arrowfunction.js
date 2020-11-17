ce5baa739a124282a0149e32c5f24b359e6d8f7f
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-17 18:15:40 +0530

    arrow fucntion more examplesadded

--- clientsidetech/javascript-examples/js/arrowfunction.js
+++ clientsidetech/javascript-examples/js/arrowfunction.js
@@ -1,13 +1,19 @@
-var add =(num1,num2)=>num1+num2;
-var output1=add(1,2);
+let add =(num1,num2)=>num1+num2;
+let output1=add(1,2);
 console.log("add output="+output1);
 
-var mul=(num1,num2)=>{
+let mul=(num1,num2)=>{
     var result=num1*num2;
     return result;
-}
+};
 
-var output2=mul(2,3);
+let output2=mul(2,3);
 console.log("mul ouput="+output2);
 
+let square=num=>num*num;
+let output3=square(10);
+console.log("square output="+output3);
 
+let greet=()=>"welcome to javascript";
+let output4=greet();
+console.log("greet message is "+output4);
