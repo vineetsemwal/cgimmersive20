310f570a04cde5a61632c919ed39cb4b1aea91a2
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-17 16:23:59 +0530

    basic examples added

---
+++ clientsidetech/javascript-examples/js/add.js
@@ -0,0 +1,8 @@
+function add(num1 ,num2){
+    var result=num1+num2;
+    return result;
+}
+
+
+var output=add(1,2);
+console.log("result is "+output);
---
+++ clientsidetech/javascript-examples/js/print.js
@@ -0,0 +1,3 @@
+var i=10;
+i="hello";
+console.log(i);
