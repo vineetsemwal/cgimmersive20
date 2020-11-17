0d3913792cf2e63e25ecd8f1299bcb393629c389
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-17 18:09:46 +0530

    arrow fucntion demo added

---
+++ clientsidetech/javascript-examples/js/arrowfunction.js
@@ -0,0 +1,13 @@
+var add =(num1,num2)=>num1+num2;
+var output1=add(1,2);
+console.log("add output="+output1);
+
+var mul=(num1,num2)=>{
+    var result=num1*num2;
+    return result;
+}
+
+var output2=mul(2,3);
+console.log("mul ouput="+output2);
+
+
