706bd00e04a01a4bfadddb2728f3fee04c22ccce
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-17 16:44:53 +0530

    double and triple equal demo

---
+++ clientsidetech/javascript-examples/js/compare.js
@@ -0,0 +1,12 @@
+
+var variable1=10;
+var variable2="10";
+var variable3=10;
+var result1=variable1==variable2;
+var result2=variable1===variable2;
+var result3=variable1===variable3;
+console.log("result 1="+result1);
+console.log("result2="+result2);
+console.log("result3 is "+result3);
+
+
