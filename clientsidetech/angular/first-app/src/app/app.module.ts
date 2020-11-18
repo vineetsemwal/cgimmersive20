5c6cfe8f1883aa55896cbe7c8948ab7d6d985d72
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-18 17:07:00 +0530

    event binding demo added

--- clientsidetech/angular/first-app/src/app/app.module.ts
+++ clientsidetech/angular/first-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventBindComponent } from './event-bind/event-bind.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventBindComponent
+    EventBindComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
