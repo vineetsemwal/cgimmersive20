d200cbee5cb0e3594433a7ea154b7e740e8cc072
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-18 16:08:06 +0530

    first demo for angular

--- clientsidetech/angular/first-app/src/app/app.component.ts
+++ clientsidetech/angular/first-app/src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title:string = 'first-app';
+
+  description:string="our first attempt at writing";
+
+  trainee:Trainee=new Trainee(1,"harshil");
+
+}
+
+export class Trainee{
+   constructor( public id:number,public name:string){
+  
+  }
+
 }
