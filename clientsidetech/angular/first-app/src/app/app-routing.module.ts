88e37563b1c07397cdc1a278835ede5d8e1180aa
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-19 19:07:20 +0530

    routing demo added

--- clientsidetech/angular/first-app/src/app/app-routing.module.ts
+++ clientsidetech/angular/first-app/src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventBindComponent } from './event-bind/event-bind.component';
+import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { StyleDemoComponent } from './style-demo/style-demo.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'two',
+    component:TwoWayComponent
+  }
+  ,
+
+  {
+   path:'property' ,
+   component:PropertyBindingComponent
+  }
+,
+{
+  path:'pipe',
+  component:PipeDemoComponent
+},
+
+{
+  path:'event',
+  component:EventBindComponent
+}
+,
+{
+  path:'style',
+  component:StyleDemoComponent
+}
+]
+
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
