06681e33fa31a01d25c295b20be9e070f32a1758
vineetsemwal <vineetsemwal82@gmail.com>
2020-11-20 12:06:09 +0530

    validations added on code driven

--- clientsidetech/angular/first-app/src/app/form-codedriven/form-codedriven.component.ts
+++ clientsidetech/angular/first-app/src/app/form-codedriven/form-codedriven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from '../model/user';
 
 @Component({
@@ -14,8 +14,8 @@ nameCtrl:FormControl;
 ageCtrl:FormControl;
 
 constructor(builder:FormBuilder){
- this.nameCtrl =builder.control('');
- this.ageCtrl=builder.control('');
+ this.nameCtrl =builder.control('',[Validators.required, Validators.minLength(2)]);
+ this.ageCtrl=builder.control('', [Validators.required]);
  this.userForm=builder.group(
 {
 fullname:this.nameCtrl,
